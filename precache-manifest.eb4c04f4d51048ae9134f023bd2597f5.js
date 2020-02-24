ac579039331d0b7df2bafd449fd78486588bb048
Armen Mkrtchian <armenmkrtchian93@gmail.com>
2020-02-24 13:11:57 +0400

    Updates

---
+++ precache-manifest.eb4c04f4d51048ae9134f023bd2597f5.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bde45a0ddc94d261bb5449ef481f18db",
+    "url": "/task_ArmBoldMind/index.html"
+  },
+  {
+    "revision": "90c6ae813fb385f030df",
+    "url": "/task_ArmBoldMind/static/css/main.9aa5329f.chunk.css"
+  },
+  {
+    "revision": "5cdb587d5a8fb7e988a0",
+    "url": "/task_ArmBoldMind/static/js/2.2f2e4b99.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/task_ArmBoldMind/static/js/2.2f2e4b99.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90c6ae813fb385f030df",
+    "url": "/task_ArmBoldMind/static/js/main.276a386e.chunk.js"
+  },
+  {
+    "revision": "b2fd7bdb765056531f07",
+    "url": "/task_ArmBoldMind/static/js/runtime-main.41bd6443.js"
+  },
+  {
+    "revision": "5feae81880575eef51f4a2868ff18194",
+    "url": "/task_ArmBoldMind/static/media/NoPath - Copy (60).5feae818.png"
+  },
+  {
+    "revision": "e3ebd58b42e5f4f9a1c9b54311f73637",
+    "url": "/task_ArmBoldMind/static/media/bookmark (1).e3ebd58b.svg"
+  },
+  {
+    "revision": "d8b708c0c687289c997266a9db0581af",
+    "url": "/task_ArmBoldMind/static/media/facebook (4).d8b708c0.svg"
+  },
+  {
+    "revision": "31a5c588d8b6ae90932ad4360b4e40a7",
+    "url": "/task_ArmBoldMind/static/media/facebook-app-symbol.31a5c588.svg"
+  },
+  {
+    "revision": "a268186e9a3496124d7ad3de641ffa8f",
+    "url": "/task_ArmBoldMind/static/media/hamburger.a268186e.svg"
+  },
+  {
+    "revision": "b529d09600bdef708e65b3f83f524c18",
+    "url": "/task_ArmBoldMind/static/media/instagram-logo (1).b529d096.svg"
+  },
+  {
+    "revision": "fcf5e8cb0f5283a0261a628f435fe6e9",
+    "url": "/task_ArmBoldMind/static/media/linkedin (1).fcf5e8cb.svg"
+  },
+  {
+    "revision": "7c3e9790486ce2cac5c19c71b973be9a",
+    "url": "/task_ArmBoldMind/static/media/linkedin-logo (1).7c3e9790.svg"
+  },
+  {
+    "revision": "40dae646294429e3acc0b4f3688459e9",
+    "url": "/task_ArmBoldMind/static/media/magnifying-glass (2).40dae646.svg"
+  },
+  {
+    "revision": "284d8cdce7fe8d4cbb4b01e9e06498af",
+    "url": "/task_ArmBoldMind/static/media/maps-and-flags (5).284d8cdc.svg"
+  },
+  {
+    "revision": "266c23cf13926cbd8246528e9aa8634a",
+    "url": "/task_ArmBoldMind/static/media/right-arrow.266c23cf.svg"
+  },
+  {
+    "revision": "a3c0c55bac4363537f55c7f01af35fc4",
+    "url": "/task_ArmBoldMind/static/media/twitter (1).a3c0c55b.svg"
+  },
+  {
+    "revision": "fea139844a42c74224b174a39a13d21e",
+    "url": "/task_ArmBoldMind/static/media/user (3).fea13984.svg"
+  }
+]);
